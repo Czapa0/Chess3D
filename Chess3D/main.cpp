72d6af6413d3ac102dedd7c1f469743394bcf3ef
Maksymilian Czapski <maks.czapski@gmail.com>
2024-08-16 14:57:31 +0200

    model class

--- Chess3D/main.cpp
+++ Chess3D/main.cpp
@@ -1,6 +1,11 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include <iostream>
+
+#include "Model.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Model m("C:\\Users\\maksc\\source\\repos\\GK1\\Projekt_4\\Chess3D\\Chess3D\\WoodenChessBoard_v1_L3.123cf3414759-151a-4f2d-be41-f4e663d384e3\\12938_WoodenChessBoard_v1_l3.obj");
+    return 0;
 }
